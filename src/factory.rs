5fd59b45f9cfd7a8df7a73b67db995475d1830c8
MVCowley <51127523+MVCowley@users.noreply.github.com>
2024-11-27 16:58:03 +0000

    feat(factory): add ndarray

--- src/factory.rs
+++ src/factory.rs
@@ -1,3 +1,7 @@
-pub fn testing() {
-    println!("Here!");
+pub fn read_factory(sampled: Vec<ndarray::Array2<u8>>, amplicons: Vec<String>) {
+    for reads in sampled {
+        for ((x, y), val) in reads.indexed_iter() {
+            
+        }
+    }
 }
