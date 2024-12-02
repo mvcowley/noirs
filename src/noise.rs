ad5a17e84b2040800b011db68764aaa0271f3931
MVCowley <51127523+MVCowley@users.noreply.github.com>
2024-12-02 14:46:07 +0000

    Correct syntax for element access

--- src/noise.rs
+++ src/noise.rs
@@ -1,7 +1,1 @@
 //! Simulate noise
-
-pub fn read_factory(sampled: Vec<ndarray::Array2<u8>>, amplicons: Vec<String>) {
-    for reads in sampled {
-        for ((x, y), val) in reads.indexed_iter() {}
-    }
-}
--- src/pcr.rs
+++ src/pcr.rs
@@ -1,8 +1,7 @@
 //! Error profiles for pcr
 
-use ndarray::{s, Array1, Array2};
-use rand::{Rng, thread_rng};
-
+use ndarray::{array, s, Array1, Array2, ArrayView1, ArrayView2};
+use rand::{thread_rng, Rng};
 
 struct SparseTree {
     matrix: Array2<u64>,
@@ -11,48 +10,46 @@ struct SparseTree {
 // SparseTree::matrix is an array of u64 as there can be 2^30 leaf nodes
 // Matrix is ones so that binary tree path calculation is easy e.g. 1*2 = 2
 impl SparseTree {
-    fn new(rounds: Vec<f32>, reads: u32) -> Self {
+    fn new(rounds: &Vec<f32>, reads: &u32) -> Self {
+        let axis1 = rounds.len() + 1;
         Self {
-            matrix: Array2::<u64>::ones((reads, rounds.len() + 1)),
+            matrix: Array2::<u64>::ones((reads as int, axis1 as int)),
         }
     }
     fn update(&mut self, read: u32, path: Array1<u64>) {
-        self.matrix.slice(s![read, ...]).assign_to(path);
+        self.matrix.slice(s![read as int]).assign_to(path);
     }
 }
 
-fn skip_round(next_state: &ArrayView2<u64> &current_node: &u64) -> bool {
-    
-}
+// If next round for other reads has the same node id, transcript did not participate in this round
+// of PCR.
+fn skip_round(next_state: &ArrayView2<u64>, current_node: &u64) -> bool {}
 
 fn calc_node(current_node: u64, child: f32) -> u64 {
     let next_node: u64 = current_node * 2;
     if child < 0.5 {
         next_node
-    }
-    else {
+    } else {
         next_node + 1
     }
 }
 
-pub fn trace_path(tree: &SparseTree, &rounds: Vec<f32>) -> Array1<u64> {
+pub fn trace_path(tree: &SparseTree, rounds: &Vec<f32>) -> Array1<u64> {
     let mut rng = thread_rng();
-    let mut path: Array1<u64> = Array1::<u64>::ones(rounds.len())
+    let mut path: Array1<u64> = Array1::<u64>::ones(rounds.len());
     for (i, efficiency) in rounds.iter().enumerate() {
         let next_state = tree.matrix.slice(s![.., i + 1]);
-        let current_node: u64 = path.slice(s![i]);
+        let current_node = path[[i]];
         let replicate: f32 = rng.gen();
         if skip_round(&next_state, &current_node) {
-            path.slice_mut(s![i +1]).assign(&current_node)
-        }
-        else {
+            path[[i + 1]] = current_node;
+        } else {
             if replicate > efficiency {
                 let child: f32 = rng.gen();
                 let next_node: u64 = calc_node(current_node, child);
-                path.slice_mut(s![i + 1]).assign(&next_node)
-            }
-            else {
-                path.slice_mut(s![i +1]).assign(&current_node)
+                path[[i + 1]] = next_node;
+            } else {
+                path[[i + 1]] = current_node;
             }
         }
     }
@@ -60,10 +57,32 @@ pub fn trace_path(tree: &SparseTree, &rounds: Vec<f32>) -> Array1<u64> {
 }
 
 pub fn construct_tree(rounds: Vec<f32>, reads: u32) -> SparseTree {
-    let mut tree = SparseTree::new(rounds, reads);
+    let mut tree = SparseTree::new(&rounds, &reads);
     for read in 0..=reads {
         let path = trace_path(&tree, &rounds);
         tree.update(read, path);
     }
     tree
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn initialise_tree() {
+        let rounds = vec![0.95, 0.95];
+        let reads = 2;
+        let tree = SparseTree::new(&rounds, &reads);
+        assert_eq!(tree.matrix, array![[1, 1], [1, 1]])
+    }
+
+    // #[test]
+    // fn create_tree() {
+    //     let rounds = vec![0.95, 0.95, 0.95];
+    //     let reads = 5;
+    //     let tree = construct_tree(rounds, reads);
+    // }
+    //
+    //
 }
