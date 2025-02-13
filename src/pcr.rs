c07c259953a21ea2e836267e44d55e6e2498fc45
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-02-13 21:19:17 +0000

    feat(pcr): make rng seedable

--- src/pcr.rs
+++ src/pcr.rs
@@ -1,9 +1,7 @@
 //! Error profiles for pcr
 
 use indexmap::IndexMap;
-
 use ndarray::prelude::*;
-use ndarray_rand::{rand_distr, RandomExt};
 use rand::Rng;
 
 /// SparseTree with matrix field that will be populated by node IDs
@@ -32,25 +30,29 @@ fn get_uniques(current_nodes: &Vec<u32>) -> Vec<u32> {
 }
 
 /// Evolve unique nodes to next round
-fn evolve_nodes(unique_nodes: &Vec<u32>, efficiency: f32) -> Array1<u32> {
-    let mut rng = rand::thread_rng();
-    let rand_arr = Array::random(unique_nodes.len(), rand_distr::Uniform::new(0., 1.));
-    let evo_arr =
-        rand_arr.map(|x| ((*x < efficiency) as u32) * 2 + (1 * (rng.gen_bool(0.5) as u32)));
+fn evolve_nodes<R: Rng + ?Sized>(
+    unique_nodes: &Vec<u32>,
+    efficiency: f32,
+    rng: &mut R,
+) -> Array1<u32> {
+    let rand_vec: Vec<f32> = (0..unique_nodes.len()).map(|_| rng.random()).collect();
+    let rand_arr = Array1::from_vec(rand_vec);
+    let evo_arr = rand_arr.map(|x| ((*x < efficiency) as u32) * 2);
     let new_nodes = Array::from_vec(unique_nodes.to_vec()) * evo_arr;
     new_nodes
 }
 
 /// Updates SparseTree object with the results of the next PCR round
-fn evolve_tree(tree: &mut SparseTree, round: usize, efficiency: f32) {
+fn evolve_tree<R: Rng + ?Sized>(tree: &mut SparseTree, round: usize, efficiency: f32, rng: &mut R) {
     let current_nodes = tree.matrix.index_axis(Axis(0), round);
     let unique_nodes = get_uniques(&current_nodes.to_vec());
-    let evolved_nodes = evolve_nodes(&unique_nodes, efficiency);
+    let evolved_nodes = evolve_nodes(&unique_nodes, efficiency, rng);
     let evolve_map: IndexMap<u32, u32> = unique_nodes
         .iter()
         .zip(evolved_nodes.iter())
         .map(|(&orig, &evolved)| (orig, evolved))
         .collect();
+    // TODO: Make updated nodes randomly be +1
     let updated_nodes = current_nodes.mapv(|node| *evolve_map.get(&node).unwrap());
     tree.matrix
         .slice_mut(s![(round + 1) as usize, ..])
@@ -58,17 +60,24 @@ fn evolve_tree(tree: &mut SparseTree, round: usize, efficiency: f32) {
 }
 
 /// Create and evolve a SparseTree with `reads` through `rounds`
-pub fn simulate_tree(efficiencies: Vec<f32>, reads: u32) -> SparseTree {
+pub fn simulate_tree<R: Rng + ?Sized>(
+    efficiencies: Vec<f32>,
+    reads: u32,
+    rng: &mut R,
+) -> SparseTree {
     let mut tree = SparseTree::new(&reads, &efficiencies);
     for round in 0..efficiencies.len() {
-        evolve_tree(&mut tree, round, efficiencies[round]);
+        evolve_tree(&mut tree, round, efficiencies[round], rng);
     }
     tree
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use rand_chacha::rand_core::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
 
     #[test]
     fn test_new_tree() {
@@ -83,5 +92,14 @@ mod tests {
         let nodes: Vec<u32> = vec![3, 2, 2, 1];
         let unique_nodes = get_uniques(&nodes);
         assert_eq!(unique_nodes, vec![1, 2, 3])
+    }
+
+    #[test]
+    fn test_evolve_nodes() {
+        let unique_nodes: Vec<u32> = vec![1, 2, 3];
+        let efficiency: f32 = 0.95;
+        let mut rng = ChaCha8Rng::seed_from_u64(11);
+        let evolved_nodes = evolve_nodes(&unique_nodes, efficiency, &mut rng);
+        assert_eq!(evolved_nodes, array![[2, 5, 7]])
     }
 }
