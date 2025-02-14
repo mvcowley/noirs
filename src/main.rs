314baa1b7285c1508babe5faa6d56b9eef065587
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-02-14 02:25:17 +0000

    feat(main): output results of initial simulations

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,12 @@ mod zipf;
 
 use rand_chacha::rand_core::SeedableRng;
 use rand_chacha::ChaCha8Rng;
+use ndarray_npy::write_npy;
 
 fn main() {
     let mut rng = ChaCha8Rng::seed_from_u64(987); // Draws [0.24346048, true, false, true]
     let efficiencies = vec![0.8; 30];
-    let tree = pcr::simulate_tree(efficiencies, 100000, &mut rng);
-    println!("{:?}", tree.matrix)
+    let tree = pcr::simulate_tree(efficiencies, 100, &mut rng);
+    println!("{:?}", tree.matrix);
+    let _ = write_npy("../out/molecule.npy", &tree.matrix);
 }
