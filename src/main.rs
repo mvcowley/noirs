e1645f89ece220481f2e55d7781406c21194d606
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-02-14 00:17:08 +0000

    feat(main): use main for rng testing

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,19 @@ mod sample;
 mod sequence;
 mod zipf;
 
-fn main() {}
+use rand::Rng;
+use rand_chacha::rand_core::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+
+fn main() {
+    let mut rng = ChaCha8Rng::seed_from_u64(987); // Draws [0.24346048, true, false, true]
+    println!("{:?}", rng.random::<f32>());
+    println!("{:?}", rng.random_bool(0.5));
+    println!("{:?}", rng.random_bool(0.5));
+    println!("{:?}", rng.random_bool(0.5));
+    println!("{:?}", rng.random::<f32>());
+    println!("{:?}", rng.random::<f32>());
+    println!("{:?}", rng.random_bool(0.5));
+    println!("{:?}", rng.random_bool(0.5));
+    println!("{:?}", rng.random_bool(0.5));
+}
