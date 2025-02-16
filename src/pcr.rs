f67a4177c38177dd5f0662945c70ea8a9e54b039
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-02-16 10:59:33 +0000

    refactor(pcr): split pcr success check and node increment operation

--- src/pcr.rs
+++ src/pcr.rs
@@ -44,8 +44,9 @@ fn evolve_nodes<R: Rng + ?Sized>(
 ) -> Array1<u32> {
     let rand_vec: Vec<f32> = (0..unique_nodes.len()).map(|_| rng.random()).collect();
     let rand_arr = Array1::from_vec(rand_vec);
-    let evo_arr = rand_arr.map(|x| ((*x < efficiency) as u32) + 1);
-    let new_nodes = Array::from_vec(unique_nodes.to_vec()) * evo_arr;
+    let evo_arr = rand_arr.map(|x| (*x < efficiency) as u32);
+    let new_nodes = Array::from_vec(unique_nodes.to_vec()) * (evo_arr + 1);
+    // let mut_arr = evo_arr.mapv(|evo| evo )
     new_nodes
 }
 
@@ -59,6 +60,7 @@ fn evolve_tree<R: Rng + ?Sized>(tree: &mut SparseTree, round: usize, efficiency:
     let current_nodes = tree.observations.index_axis(Axis(1), round);
     let unique_nodes = get_uniques(&current_nodes.to_vec());
     let evolved_nodes = evolve_nodes(&unique_nodes, efficiency, rng);
+    // let mutated_nodes = evolve_nodes(&evolved_nodes, error, rng);
     let evolve_map: IndexMap<u32, u32> = unique_nodes
         .iter()
         .zip(evolved_nodes.iter())
@@ -69,18 +71,18 @@ fn evolve_tree<R: Rng + ?Sized>(tree: &mut SparseTree, round: usize, efficiency:
         let evolved = is_non_zero(new - &node);
         new + (evolved * (rng.random_bool(0.5) as u32))
     });
-    let mutations = current_nodes.mapv(|node| {
-        let new = *evolve_map.get(&node).unwrap();
-        let evolved = is_non_zero(new - &node);
-        // TODO: Allow for multiple mutations per round: draw from distribution
-        (evolved * (rng.random_bool(error) as u32)) as u8
-    });
+    // let mutations = current_nodes.mapv(|node| {
+    //     let new = *evolve_map.get(&node).unwrap();
+    //     let evolved = is_non_zero(new - &node);
+    //     // TODO: Allow for multiple mutations per round: draw from distribution
+    //     (evolved * (rng.random_bool(error) as u32)) as u8
+    // });
     tree.observations
         .slice_mut(s![.., (round + 1) as usize])
         .assign(&updated_nodes);
-    tree.mutations
-        .slice_mut(s![.., (round + 1) as usize])
-        .assign(&mutations);
+    // tree.mutations
+    //     .slice_mut(s![.., (round + 1) as usize])
+    //     .assign(&mutations);
 }
 
 /// Create and evolve a SparseTree with `reads` through `rounds`
@@ -117,7 +119,7 @@ mod tests {
     fn test_get_uniques() {
         let nodes: Vec<u32> = vec![3, 2, 2, 1];
         let unique_nodes = get_uniques(&nodes);
-        assert_eq!(unique_nodes, vec![1, 2, 3])
+        assert_eq!(unique_nodes, vec![1, 2, 3]);
     }
 
     #[test]
@@ -126,7 +128,7 @@ mod tests {
         let efficiency: f32 = 0.5;
         let mut rng = ChaCha8Rng::seed_from_u64(927); // Draws [0.4124102, 0.7353993, 0.8147212]
         let evolved_nodes = evolve_nodes(&unique_nodes, efficiency, &mut rng);
-        assert_eq!(evolved_nodes, array![2, 2, 3])
+        assert_eq!(evolved_nodes, array![2, 2, 3]);
     }
 
     #[test]
@@ -136,15 +138,16 @@ mod tests {
         assert_eq!(is_non_zero(2), 1);
     }
 
-    // #[test]
-    // fn test_evolve_tree_rep_success() {
-    //     let efficiencies = vec![0.5, 0.5];
-    //     let mut tree = SparseTree::new(&3, &efficiencies);
-    //     let mut rng = ChaCha8Rng::seed_from_u64(987); // Draws [0.24346048, true, false, true]
-    //     let round = 0;
-    //     evolve_tree(&mut tree, round, efficiencies[round], &mut rng);
-    //     assert_eq!(tree.observations, array![[1, 3, 1], [1, 2, 1], [1, 3, 1]]);
-    // }
+    #[test]
+    fn test_evolve_tree_rep_success() {
+        let efficiencies = vec![0.5, 0.5];
+        let errors = vec![0.5, 0.5];
+        let mut tree = SparseTree::new(&3, &efficiencies);
+        let mut rng = ChaCha8Rng::seed_from_u64(987); // Draws [0.24346048, true, false, true]
+        let round = 0;
+        evolve_tree(&mut tree, round, efficiencies[round], errors[round], &mut rng);
+        assert_eq!(tree.observations, array![[1, 3, 1], [1, 2, 1], [1, 3, 1]]);
+    }
 
     // #[test]
     // fn test_evolve_tree_rep_fail() {
