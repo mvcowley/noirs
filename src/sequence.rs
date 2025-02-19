4ecb851828188c679c599e53ad39eb3ab5241ba3
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-02-19 03:57:48 +0000

    fix(sequence): pass array view to sequencer test

--- src/sequence.rs
+++ src/sequence.rs
@@ -2,7 +2,7 @@
 
 use ndarray::prelude::*;
 
-use crate::pcr::PcrParameters;
+use crate::pcr::Reaction;
 use rand::Rng;
 use rand_distr::{Binomial, Distribution};
 
@@ -14,14 +14,13 @@ pub struct Sequencer {
 
 pub fn sequence<R: Rng + ?Sized>(
     library: &ArrayView1<u32>,
-    reaction: &PcrParameters,
+    reaction: &Reaction,
     sequencer: Sequencer,
     rng: &mut R,
 ) -> Array1<u32> {
     let bin = Binomial::new(reaction.sites as u64, sequencer.error).unwrap();
     library.mapv(|_| bin.sample(rng).try_into().unwrap())
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -31,17 +30,15 @@ mod tests {
 
     #[test]
     fn test_sequence() {
-        let library = array![4, 5, 3];
-        let reaction = PcrParameters {
+        let library: Array1<u32> = array![4, 5, 3];
+        let reaction = Reaction {
             sites: 3,
             efficiencies: vec![0.95; 2],
             errors: vec![0.5; 2],
         };
-        let sequencer = Sequencer {
-            error: 0.5
-        };
+        let sequencer = Sequencer { error: 0.5 };
         let mut rng = ChaCha8Rng::seed_from_u64(927); // Draws [2, 1, 3, 2, 1, 0] from binomial with n=3 and p=0.5
-        let sequence_errors = sequence(&library, &reaction, sequencer, &mut rng);
+        let sequence_errors = sequence(&library.slice(s![..]), &reaction, sequencer, &mut rng);
         assert_eq!(sequence_errors, array![2, 1, 3]);
     }
 }
