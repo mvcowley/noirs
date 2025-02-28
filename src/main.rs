6e73e5afb6547cf72c47e8f5f174151cc3d202e8
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-02-28 22:58:57 +0000

    feat(presentation): testing variable mutation rates

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,36 @@ use rand_chacha::rand_core::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 
 fn main() {
-    // let mut rng = ChaCha8Rng::seed_from_u64(987); // run 1
+    let mut rng = ChaCha8Rng::seed_from_u64(987); // run 1
     // let mut rng = ChaCha8Rng::seed_from_u64(392); // run 2
-    let mut rng = ChaCha8Rng::seed_from_u64(654); // run 3
+    // let mut rng = ChaCha8Rng::seed_from_u64(654); // run 3
 
     // Setup sampler
     let total_observations = 1_000_000;
     let max_observations = 1e3;
-    let exponent = 1.5;
+    let exponent = 2.0;
     let sampler = sample::Sampler::new(total_observations, max_observations, exponent);
+
+    let mut pcr1 = vec![0.1; 10];
+    let mut pcr2 = vec![0.0; 10];
+    pcr1.append(&mut pcr2);
 
     // Setup PCR reaction
     let reaction = pcr::Reaction {
         sites: 12,
-        efficiencies: vec![0.8; 30],
-        errors: vec![0.001; 30],
+        efficiencies: vec![0.95; 20],
+        errors: pcr1,
     };
 
     // Setup sequencer
     let sequencer = sequence::Sequencer { error: 0.005 };
 
     // Sample library
-    sample::sample(&sampler, &reaction, &sequencer, &mut rng, "../out/noirs_out_3_high_error/");
+    sample::sample(
+        &sampler,
+        &reaction,
+        &sequencer,
+        &mut rng,
+        "../out/noirs_out_error_step/",
+    );
 }
